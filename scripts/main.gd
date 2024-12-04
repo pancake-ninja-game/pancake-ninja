f97b31b594110a5cbf81b1743931e798a12c3a11
Viktor Mörsell <viktor@upper.st>
2024-12-04 20:19:59 +0100

    Add unwinding pancake

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,43 +1,41 @@
 extends Node2D
 
 @export var sheet_scene: PackedScene
-@export var sheet_spacing: float = 64
-@export var spawn_speed: float = 1.0
 @export var screen_width: float = 1024
-@export var gravity_floor: float = 600
 
+var sheet_spacing: float = 0
+var sheet_speed: float = 200
 var sheets = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Timer.wait_time = spawn_speed
+	# Dynamically calculate sheet spacing
+	var tmp_sheet = sheet_scene.instantiate()
+	add_child(tmp_sheet)
+	sheet_spacing = tmp_sheet.sheet_width + 10
+	sheet_speed = tmp_sheet.move_speed
+	tmp_sheet.queue_free()
+	
+	# Set the timer interval
+	var spawn_interval = sheet_spacing / sheet_speed
+	$Timer.wait_time = spawn_interval
 	$Timer.start()
-	
-func _on_Timer_timeout():
-	spawn_sheets()
 
-func spawn_sheets():
-	print("spawn")
-	var new_sheet = sheet_scene.instantiate()
-	if sheets.size() > 0:
-		# Place new sheet next to the last one
-		new_sheet.position.x = sheets[-1].position.x + sheet_spacing
-	else:
-		new_sheet.position.x = 0
+func _on_Timer_timeout():
+	spawn_sheet()
 	
-	new_sheet.position.y = 100
-	add_child(new_sheet)
-	sheets.append(new_sheet)
-	
-	# Set the connection between sheets
-	for i in range(1, len(sheets)):
-		sheets[i-1].connected_sheets.append(sheets[i])
+func spawn_sheet():
+	var s = sheet_scene.instantiate()
+	s.position = Vector2(-sheet_spacing, 100)
+	add_child(s)
+	sheets.append(s)
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # Handle resetting sheets when reaching the right screen boundary
 func _process(delta: float) -> void:
+	# Remove sheets that move off-screen to the right
 	for s in sheets:
-		if s.position.x > screen_width:
+		if s.position.x > screen_width + sheet_spacing:
 			remove_sheet(s)
 
 func remove_sheet(s):
--- scripts/sheet.gd
+++ scripts/sheet.gd
@@ -1,12 +1,23 @@
 extends Node2D
 
+@export var move_speed: float = 200
+var sheet_width: float = 0
+
 var falling = false
 var fall_speed = 200
 var gravity = 500
 var connected_sheets = []
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var sprite = $Sprite2D
+	if sprite.texture:
+		sheet_width = sprite.texture.get_width()
+			
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	position.x += move_speed * delta
+	
 	if falling:
 		position.y += fall_speed * delta
 		fall_speed += gravity * delta
@@ -18,12 +29,6 @@ func trigger_fall():
 	falling = true
 	for s in connected_sheets:
 		s.trigger_fall()
-
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 
 func _input_event(viewport, event, shape_idx):
