e019e0b6ef77a0fd13a97f074a1d27b3c7ba6f51
Viktor Mörsell <viktor@upper.st>
2024-12-04 20:47:02 +0100

    Add some randomness to how sheets fall

--- scripts/sheet.gd
+++ scripts/sheet.gd
@@ -3,15 +3,24 @@ extends Node2D
 signal sheet_fall_triggered
 
 @export var move_speed: float = 200
+@export var gravity: float = 2000
+@export var rotation_speed: float = 10
+@export var skew_speed: float = 3
+
 var sheet_width: float = 0
 
 var falling = false
 var fall_speed = 0
-var gravity = 500
+var rotation_direction: float = 0
+var skew_direction: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("mouse_entered", Callable(self, "_on_mouse_entered"))
+	
+	# Randomize drift and rotation direction
+	rotation_direction = randf_range(-rotation_speed, rotation_speed)
+	skew_direction = randf_range(-skew_speed, skew_speed)
 	
 	var sprite = $Sprite2D
 	if sprite.texture:
@@ -24,6 +33,9 @@ func _process(delta):
 	if falling:
 		fall_speed += gravity * delta
 		position.y += fall_speed * delta
+		
+		rotation += rotation_direction * delta
+		skew += skew_direction * delta
 
 func trigger_fall():
 	falling = true
