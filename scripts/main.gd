bf460c47a012cda630f58d664073f5c076da961b
Viktor Mörsell <viktor@upper.st>
2024-12-04 22:34:56 +0100

    Add metrics

--- scripts/main.gd
+++ scripts/main.gd
@@ -8,10 +8,14 @@ var pancake_y_pos = 370
 
 var sheet_spacing: float = 0
 var sheet_speed: float = 200
+var stack_capacity = 6
 
 var pancake = []
 var stacked_sheets = []
 var scrapped_sheets = []
+
+var ok_cut_timestamps = []
+var ng_cut_timestamps = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -27,6 +31,9 @@ func _ready() -> void:
 	$Timer.wait_time = spawn_interval
 	$Timer.start()
 
+func set_score(yield_, ppm):
+	pass
+	
 func _on_Timer_timeout():
 	spawn_sheet()
 	
@@ -39,18 +46,21 @@ func spawn_sheet():
 	pancake.append(s)
 
 func stack_sheet(s):
-	if stacked_sheets.size() == 10:
+	if stacked_sheets.size() == stack_capacity:
 		flush_stacked_sheets()
 		
 	pancake.erase(s)
 	stacked_sheets.append(s)
 	s.stack_in_jr()
 	
+	ok_cut_timestamps.append(Time.get_ticks_msec())
+	
 func scrap_sheets_until(s):
 	var idx = pancake.find(s) + 1
 	
 	for sheet in pancake.slice(0, idx):
 		sheet.scrap()
+		ng_cut_timestamps.append(Time.get_ticks_msec())
 	
 	scrapped_sheets.append_array(pancake.slice(0, idx))
 	pancake = pancake.slice(idx)
@@ -83,6 +93,8 @@ func _on_ng_cut_detected(triggered_sheet):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # Handle resetting sheets when reaching the right screen boundary
 func _process(delta: float) -> void:
+	update_scores()
+	
 	# Remove sheets that move off-screen to the right
 	for sheet in pancake:
 		if sheet.position.x > screen_width + sheet_spacing:
@@ -98,6 +110,26 @@ func remove_sheet(s):
 	pancake.erase(s)
 	s.queue_free()
 
+func update_scores():
+	var current_time = Time.get_ticks_msec()
+	
+	var sample_seconds = 10
+	
+	ok_cut_timestamps = ok_cut_timestamps.filter(func(timestamp): return current_time - timestamp <= sample_seconds * 1000)
+	ng_cut_timestamps = ng_cut_timestamps.filter(func(timestamp): return current_time - timestamp <= sample_seconds * 1000)
+	
+	var yield_pct = 0.0
+	
+	var ok_cuts = len(ok_cut_timestamps)
+	var ng_cuts = len(ng_cut_timestamps)
+	if ok_cuts + ng_cuts > 0:
+		yield_pct = float(ok_cuts) / (ok_cuts + ng_cuts) * 100
+	
+	var ppm = 0
+	ppm = ok_cuts * 60 / sample_seconds / stack_capacity
+	
+	$CanvasLayer/ScoreLabel.text = "Yield: %d%%\nPPM: %d" % [yield_pct, ppm]
+	
 # Debug
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed:
